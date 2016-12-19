ef3904ddf76d36caf7c26b2d0709c4761d769007
harshit sinha <coollumia@outlook.com>
2016-12-19 17:39:06 +0530

    optimixation sw

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var gulp = require('gulp');
+require('./gulpfile.js');
 var proxy = require('http-proxy-middleware');
 
 var app = express();
-
+gulp.start("generate-service-worker");
 app.use('/api/*', proxy({target: 'https://www.swiggy.com', changeOrigin: true}));
 
 app.use(express.static('www'));
