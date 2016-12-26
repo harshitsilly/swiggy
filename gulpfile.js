34699973b8117064f5cddd1f42e983ebdc21572e
harshit sinha <coollumia@outlook.com>
2016-12-26 22:39:28 +0530

    code delete

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('generate-service-worker', function(callback) {
   var rootDir = 'www';
 
   swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    staticFileGlobs: ['!./www/lib/*',rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
     stripPrefix: rootDir,
     maximumFileSizeToCacheInBytes: 1024 * 1024 * 5
   }, callback);
